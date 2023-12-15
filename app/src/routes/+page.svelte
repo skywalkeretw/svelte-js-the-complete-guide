87f7c00f92b4dde52d235e0c18289d9734aab3e7
Luke Roy <lukeroy@outlook.com>
2023-12-15 10:55:46 +0100

    more reactivity

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -5,28 +5,41 @@
     let title = "";
     let image = "";
     let description = "";
-    let formState = 'empty'
-
-    let createdContacts = [] 
+    let formState = "empty";
+  
+    let createdContacts = [];
+  
     function addContact() {
-        if (name.trim().length == 0 || title.trim().length == 0 || image.trim().length == 0 || description.trim().length == 0) {
-            formState = 'invalid';
-            return;
+      // event.preventDefault();
+      if (
+        name.trim().length == 0 ||
+        title.trim().length == 0 ||
+        image.trim().length == 0 ||
+        description.trim().length == 0
+      ) {
+        formState = "invalid";
+        return;
+      }
+  
+      createdContacts = [
+        ...createdContacts,
+        {
+          id: Math.random(),
+          name: name,
+          jobTitle: title,
+          imageUrl: image,
+          desc: description
         }
-        formState = 'done'
-        createdContacts = [...createdContacts, {
-            id: Math.random(), 
-            name,
-            title,
-            image,
-            description
-        }]
+      ];
+      formState = "done";
     }
+  
     function deleteFirst() {
-        createdContacts = createdContacts.slice(1)
+      createdContacts = createdContacts.slice(1);
     }
+  
     function deleteLast() {
-        createdContacts = createdContacts.slice(0,-1)
+      createdContacts = createdContacts.slice(0, -1);
     }
   </script>
   
@@ -34,10 +47,11 @@
     #form {
       width: 30rem;
       max-width: 100%;
+      margin: 1rem 0;
     }
   </style>
   
-  <div id="form">
+  <form id="form">
     <div class="form-control">
       <label for="userName">User Name</label>
       <input type="text" bind:value={name} id="userName" />
@@ -54,23 +68,29 @@
       <label for="desc">Description</label>
       <textarea rows="3" bind:value={description} id="desc" />
     </div>
-  </div>
-
-<button on:click={addContact}>Add Card</button>
-<button on:click={deleteFirst}>Delete First</button>
-<button on:click={deleteLast}>Delete Last</button>
-{#if formState == 'invalid'}
-    <p>invalid input</p>
-{:else}
-<p>add data</p>
-{/if}
-
-{#each createdContacts as createdContact, index (createdContact.id)}
-<div>
-    <h2># {index}</h2>
-    <ContactCard userName={createdContact.name} jobTitle={createdContact.title} description={createdContact.description} userImage={createdContact.image} />
-</div>
-
-{:else}
-<p>please start adding contacts</p>
-{/each}
+    <button on:click|preventDefault={addContact} type="submit">Add Contact Card</button>
+  </form>
+  
+  <button
+    on:click={event => createdContacts = createdContacts.slice(1)}>
+    Delete First
+  </button>
+  <button on:click={deleteLast}>Delete Last</button>
+  
+  {#if formState === 'invalid'}
+    <p>Invalid input.</p>
+  {:else}
+    <p>Please enter some data and hit the button!</p>
+  {/if}
+  
+  {#each createdContacts as contact, i (contact.id)}
+    <h2># {i + 1}</h2>
+    <ContactCard
+      userName={contact.name}
+      jobTitle={contact.jobTitle}
+      description={contact.desc}
+      userImage={contact.imageUrl} />
+  {:else}
+    <p>Please start adding some contacts, we found none!</p>
+  {/each}
+  
